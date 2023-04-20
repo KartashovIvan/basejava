f711ba90c3f43312cf6fd6ffee0ad2e0d983db37
Kartashov Ivan <vano.noname69@yandex.ru>
2023-04-20 13:28:04 +0300

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,70 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int index =0;index<size();index++){
+            if (storage[index] != null){
+                storage[index] = null;
+            }
+        }
     }
 
     void save(Resume r) {
+        int index =0;
+
+        for(;index<storage.length;index++){
+            if (storage[index] == null){
+                break;
+            }
+        }
+
+        storage[index] = r;
     }
 
     Resume get(String uuid) {
+        for(int index = 0;index<size();index++){
+            if (uuid.equals(storage[index].uuid)){
+                return storage[index];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = 0;
+
+        for(;index<size();index++){
+            if (uuid.equals(storage[index].uuid)){
+                break;
+            }
+        }
+
+        for (;index<size();index++){
+            storage[index] = storage[index+1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] all = new Resume[size()];
+
+        for (int index =0;index< all.length;index++){
+            all[index] = storage[index];
+        }
+
+        return all;
     }
 
     int size() {
-        return 0;
+        int indexSize =0;
+
+        for(;indexSize<storage.length;indexSize++){
+            if (storage[indexSize] == null){
+                break;
+            }
+        }
+
+        return indexSize;
     }
 }
