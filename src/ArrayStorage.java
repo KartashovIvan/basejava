ab2860050e9cfff29df136790ac275a655f8ea37
Kartashov Ivan <vano.noname69@yandex.ru>
2023-04-22 11:42:01 +0300

    Исправлен метод "size". Размер массива вынесен в переменную класса.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,6 +3,7 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private static int sizeArrayResume = 0;
 
     void clear() {
         for (int index = 0; index < size(); index++) {
@@ -10,25 +11,25 @@ public class ArrayStorage {
                 storage[index] = null;
             }
         }
+        sizeArrayResume = 0;
     }
 
     void save(Resume r) {
-
         for (int index = 0; index < storage.length; index++) {
             if (storage[index] == null) {
                 storage[index] = r;
+
+                sizeArrayResume++;
             }
 
             if (r.uuid.equals(storage[index].uuid)) {
                 break;
             }
         }
-
-
     }
 
     Resume get(String uuid) {
-        for (int index = 0; index < size(); index++) {
+        for (int index = 0; index < sizeArrayResume; index++) {
             if (uuid.equals(storage[index].uuid)) {
                 return storage[index];
             }
@@ -39,14 +40,16 @@ public class ArrayStorage {
     void delete(String uuid) {
         int index = 0;
 
-        for (; index < size(); index++) {
+        for (; index < sizeArrayResume; index++) {
             if (uuid.equals(storage[index].uuid)) {
                 break;
             }
         }
 
-        for (; index < size(); index++) {
+        for (; index < sizeArrayResume; index++) {
             storage[index] = storage[index + 1];
+
+            sizeArrayResume--;
         }
     }
 
@@ -54,7 +57,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] all = new Resume[size()];
+        Resume[] all = new Resume[sizeArrayResume];
 
         for (int index = 0; index < all.length; index++) {
             all[index] = storage[index];
@@ -64,14 +67,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int indexSize = 0;
-
-        for (; indexSize < storage.length; indexSize++) {
-            if (storage[indexSize] == null) {
-                break;
-            }
-        }
-
-        return indexSize;
+        return sizeArrayResume;
     }
 }
