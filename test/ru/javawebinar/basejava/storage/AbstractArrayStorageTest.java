4ac56f7808cf14a69937da63cb02a69fcc4679d1
Kartashov Ivan <vano.noname69@yandex.ru>
2023-05-06 00:34:34 +0300

    HW4 В методе MainReflection вызван метод toString класса Resume (17 и 18 строки) Написаны тесты для AbstractArrayStorage, SortedArrayStorage, ArrayStorage

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -3,38 +3,82 @@ package ru.javawebinar.basejava.storage;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorageTest {
-    private Storage storage;
+    private final Storage storage;
 
     private static final String UUID_1 = "uuid1";
+    private static final Resume RESUME_1 = new Resume(UUID_1);
     private static final String UUID_2 = "uuid2";
+    private static final Resume RESUME_2 = new Resume(UUID_2);
     private static final String UUID_3 = "uuid3";
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final String UUID_4 = "uuid4";
+    private static final Resume RESUME_4 = new Resume(UUID_4);
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         storage.clear();
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_2));
-        storage.save(new Resume(UUID_3));
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
     }
 
     @Test
-    public void clear() {
+    public void clearStorage() {
+        storage.clear();
+        Assert.assertEquals(0, storage.size(), 0);
     }
 
     @Test
-    public void save() {
+    public void saveNewResume() {
+        storage.save(RESUME_4);
+        Assert.assertEquals(4, storage.size());
     }
 
     @Test
-    public void update() {
+    public void saveOverflowStorage() {
+        try {
+            storage.save(RESUME_4);
+        } catch (StorageException se) {
+            Assert.fail("Overflow happened ahead of time");
+        }
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void saveExistResume() {
+        storage.save(RESUME_1);
+        Assert.assertEquals(3, storage.size());
     }
 
     @Test
-    public void get() {
+    public void updateResume() {
+        Resume r = new Resume("uuid3");
+        storage.update(r);
+        Assert.assertNotSame(RESUME_3, r);
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void updateNotExistResume() {
+        Resume r = new Resume(UUID_4);
+        storage.update(r);
+    }
+
+    @Test
+    public void getResume() {
+        Assert.assertSame(RESUME_1, storage.get("uuid1"));
+        Assert.assertSame(RESUME_2, storage.get("uuid2"));
+        Assert.assertSame(RESUME_3, storage.get("uuid3"));
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -43,15 +87,25 @@ public abstract class AbstractArrayStorageTest {
     }
 
     @Test
-    public void delete() {
+    public void deleteResume() {
+        storage.delete(UUID_2);
+        Assert.assertEquals(2, storage.size());
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExistResume() {
+        storage.delete(UUID_4);
+        Assert.assertEquals(3, storage.size());
     }
 
     @Test
     public void getAll() {
+        Resume[] r = Arrays.copyOfRange(storage.getAll(), 0, storage.size());
+        Assert.assertArrayEquals(r, storage.getAll());
     }
 
     @Test
-    public void size() {
-        Assert.assertEquals(3,storage.size());
+    public void sizeResumeStorage() {
+        Assert.assertEquals(3, storage.size(), 3);
     }
 }
